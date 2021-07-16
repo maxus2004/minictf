a0c608b431ed2859cfc0e8b04f325cd8553e2b2d
maxus2004 <65509668+maxus2004@users.noreply.github.com>
2021-07-16 13:30:13 +0300

    update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -9,10 +9,20 @@ def mainPage():
     return 'lolol'
 
 def cezar(text,rot):
-    
+   result = ""
+   for i in range(len(text)):
+      char = text[i]
+      if (char.isupper()):
+         result += chr((ord(char) + s-65) % 26 + 65)
+      else:
+         result += chr((ord(char) + s - 97) % 26 + 97)
+      return result
 
 @app.route('/task1', methods=['GET','POST'])
 def task1():
+    text='хеллоу ворлд'
+    rot=3
+    
     correct = False
     if request.method='POST':
         answer = request.form['answer']
