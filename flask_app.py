5c72aff32f2a492cf63197219e6d099d95f6978b
maxus2004 <65509668+maxus2004@users.noreply.github.com>
2021-07-16 07:57:34 +0300

    update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
@@ -8,7 +9,7 @@ def index():
 
 @app.route('/task1')
 def index():
-    return 'task 1'
+    return render_template('task1.html')
 
 if __name__ == '__main__':
     app.run()
