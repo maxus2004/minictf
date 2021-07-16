76873d13d34fad759bfe8443b270a7ec4f22ffb8
maxus2004 <65509668+maxus2004@users.noreply.github.com>
2021-07-16 10:22:40 +0300

    update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -10,10 +10,6 @@ def mainPage():
 @app.route('/task1')
 def task1():
     return render_template('task1.html', rot=2, encrypted='лололоо')
-    
-@app.route('/style.css')
-def style_css():
-    return send_static_file('style.css')
 
 if __name__ == '__main__':
     app.run()
