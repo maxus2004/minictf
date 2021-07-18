d452cef1ba654bd9dcc82c5c927cd7e3b99892f2
maxus2004 <65509668+maxus2004@users.noreply.github.com>
2021-07-18 08:30:21 +0300

    update console.js

--- static/console.js
+++ static/console.js
@@ -64,5 +64,5 @@ function changed(console) {
     }else if(text.length-text.lastIndexOf('\n') < dir.substring(dir.lastIndexOf('/')+1).length+3){
         console.value+=' '
     }
-    console.selectionEnd = console.value.length
+    console.selectionStart = console.value.length
 }
