a796b7ce777622aed458fe391904c4fa458f8be9
maxus2004 <65509668+maxus2004@users.noreply.github.com>
2021-07-19 14:28:02 +0300

    update xss.js

--- static/xss.js
+++ static/xss.js
@@ -1,4 +1,7 @@
 function send(){
     text = document.getElementById('textInput').value
     name = document.getElementById('nameInput').value
+    
+    
+    comment = '                <div class="text">'+text+'</div><div class="name">'+name+'</div>'
 }
