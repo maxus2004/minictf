2b33dd0116f9229a2d7b6c8bbba8062d808d4994
maxus2004 <65509668+maxus2004@users.noreply.github.com>
2021-07-19 14:53:15 +0300

    update xss.js

--- static/xss.js
+++ static/xss.js
@@ -1,6 +1,8 @@
 function send(){
-    text = ""+(document.getElementById('textInput').value).replace(/[<>]/g, '"')
-    name = ""+document.getElementById('nameInput').value
+    text = document.getElementById('textInput').value
+    name = document.getElementById('nameInput').value
+    
+    text = text.replace(/[<>]/g, '"')
     
     
     comment = '<div class="comment"><div class="text">'+text+'</div><div class="name">'+name+'</div></div>'
