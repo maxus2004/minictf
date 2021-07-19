4c2d5c5c97b5cbf016b55bd3305401f347898978
maxus2004 <65509668+maxus2004@users.noreply.github.com>
2021-07-19 14:28:32 +0300

    update xss.js

--- static/xss.js
+++ static/xss.js
@@ -4,4 +4,5 @@ function send(){
     
     
     comment = '                <div class="text">'+text+'</div><div class="name">'+name+'</div>'
+    document.getElementById('comments').innerHtml+=comment
 }
