19d9e133e31befd699adabf64a99a1a0d7c770b3
maxus2004 <65509668+maxus2004@users.noreply.github.com>
2021-07-20 12:42:02 +0300

    update xss.js

--- static/xss.js
+++ static/xss.js
@@ -11,3 +11,7 @@ function send(){
     comment = '<div class="comment"><div class="text">'+text+'</div><div class="name">'+name+'</div></div>'
     document.getElementById('comments').innerHTML+=comment
 }
+
+function answer(){
+    alert('Ответ: пожилая мышь')
+}
